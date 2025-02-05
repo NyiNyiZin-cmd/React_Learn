6b0c71dbdb0e18dbf0f0e1c3c970c55f23063b85
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-02-05 17:37:25 +0900

    to deploy setup

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  base: '/React_Learn',
+  plugins: [react(),tailwindcss()],
   server: {
     port: 5138,
     open: true,
